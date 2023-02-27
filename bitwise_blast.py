3400722bbf9814c20c08208243d90a80464599d3
Fabrizio Golino <info.fabriziogolino@gmail.com>
2023-02-27 14:00:16 +0100

    edited bitwise_blast.py

--- bitwise_blast.py
+++ bitwise_blast.py
@@ -1,48 +1,53 @@
 import random
 
+# Generate random 5 bits binary number and name it as table number
+table_number = bin(random.randint(0, 31))[2:].zfill(5)
 
-def generate_secret_number():
-    """Generate a random binary number represented as a string of asterisks"""
-    secret_number = bin(random.randint(0, 255))[2:].zfill(8)
-    return "".join("*" if b == "0" else b for b in secret_number)
+# Generate random 1 bit binary number for opponent and player
+opponent_number = bin(random.randint(0, 1))[2:]
+player_number = bin(random.randint(0, 1))[2:]
 
+# Replace table_number with asterisks for initial display
+displayed_table_number = "*" * len(table_number)
 
-def apply_operator(operator, number):
-    """Apply a bitwise operator to a binary number"""
-    if operator == "&":
-        return bin(int(number, 2) & random.randint(0, 255))[2:].zfill(8)
-    elif operator == "|":
-        return bin(int(number, 2) | random.randint(0, 255))[2:].zfill(8)
-    elif operator == "^":
-        return bin(int(number, 2) ^ random.randint(0, 255))[2:].zfill(8)
+# Keep track of number of matches and misses
+matches = 0
+misses = 0
+
+# Loop until there are 3 matches or 3 misses
+while matches < 3 and misses < 3:
+    # Display opponent number, player number, and table number (with some digits replaced by asterisks)
+    print(f"Opponent number: {opponent_number}")
+    print(f"Table number: {displayed_table_number}")
+    print(f"Player number: {player_number}")
+
+    # Get the player's choice of bitwise operator
+    operator = input("Choose a bitwise operator (&, |, ^): ")
+
+    # Apply the chosen operator to the opponent number and player number
+    result = eval(f"{opponent_number} {operator} {player_number}")
+
+    # Check if the result matches the corresponding bit in the table number
+    bit_index = len(table_number) - (matches + misses + 1)
+    if result == int(table_number[bit_index]):
+        # If there is a match, replace the corresponding asterisk with the matched bit
+        displayed_table_number = displayed_table_number[:bit_index] + str(
+            table_number[bit_index]) + displayed_table_number[bit_index+1:]
+        print(f"Correct! The bit was {table_number[bit_index]}")
+        matches += 1
     else:
-        raise ValueError("Invalid operator")
+        # If there is a miss, replace the corresponding asterisk with an X
+        displayed_table_number = displayed_table_number[:bit_index] + \
+            "X" + displayed_table_number[bit_index+1:]
+        print(f"Wrong! The bit was {table_number[bit_index]}")
+        misses += 1
 
+    # Generate new random 1 bit binary number for opponent and player and display the updated table number
+    opponent_number = bin(random.randint(0, 1))[2:]
+    player_number = bin(random.randint(0, 1))[2:]
 
-def reveal_bits(secret_number, player_number):
-    """Compare the bits of the secret number and the player number, revealing the matching bits"""
-    revealed_number = ""
-    for i in range(len(secret_number)):
-        if secret_number[i] == player_number[i]:
-            revealed_number += secret_number[i]
-        else:
-            revealed_number += "*"
-    return revealed_number
-
-
-# Main game loop
-while True:
-    secret_number = generate_secret_number()
-    player_number = bin(random.randint(0, 255))[2:].zfill(8)
-    print("Secret number:", secret_number)
-    print("Player number:", player_number)
-    operator = input("Enter a bitwise operator (&, |, ^): ")
-    player_number = apply_operator(operator, player_number)
-    revealed_number = reveal_bits(secret_number, player_number)
-    print("Revealed number:", revealed_number)
-    if revealed_number == secret_number:
-        print("You win! Sercret number was:", secret_number)
-        break
-    else:
-        input("Press Enter to continue...")
-#
+print("Game over!")
+if matches >= 3:
+    print("Congratulations! You won! Table number was: " + table_number)
+else:
+    print("Sorry, you lost. Better luck next time! " + table_number)
